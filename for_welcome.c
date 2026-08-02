1cd6a9ddff24ac2f934c9b51df11d94abce0759f
shoaib <officialxshoaibmac@gmail.com>
2026-08-03 00:29:12 +0530

    adding my project to git

---
+++ for_welcome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+char welcome[50];
+
+printf("Enter your name: ");
+scanf("%s", welcome);
+
+printf("welcome %s to shoaib company", welcome);
+
+
+return 0;
+}
+
---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+char first_name[50], last_name[50], gender[10];
+int age;
+
+printf("Enter your details\n");
+
+printf(" first name:");
+scanf("%s", &first_name);
+
+printf(" last name:");
+scanf("%s", &last_name);
+
+printf(" age:");
+scanf("%d", &age);
+
+printf(" gender:");
+scanf(" %s \n", &gender);
+
+
+
+printf("Name: %s %s \n", first_name , last_name);
+
+printf("Age: %d\n", age);
+printf("Gender: %s\n", gender);
+
+  return 0;
+}
